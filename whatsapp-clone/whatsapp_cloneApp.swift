edd6daad2bbefc5fa158b2cb56e7165de7c6ed9c
Radu Petrisel <radupetrisel@gmail.com>
2024-04-18 20:17:47 +0300

    Initial Commit

---
+++ whatsapp-clone/whatsapp_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  whatsapp_cloneApp.swift
+//  whatsapp-clone
+//
+//  Created by Radu Petrisel on 18.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct whatsapp_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
