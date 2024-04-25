b843bc0924ba8d4815c568378b8fcd05025ab08a
Radu Petrisel <radupetrisel@gmail.com>
2024-04-25 20:43:45 +0300

    Add WelcomeView

---
+++ whatsapp-clone/Core/Authentication/View/WelcomeView.swift
@@ -0,0 +1,70 @@
+//
+//  WelcomeView.swift
+//  whatsapp-clone
+//
+//  Created by Radu Petrisel on 25.04.2024.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    @State private var selectedLanguage = "English"
+    
+    var body: some View {
+        VStack(spacing: 30) {
+            Image(.welcome)
+                .resizable()
+                .frame(maxWidth: .infinity)
+                .scaledToFit()
+                .padding(.horizontal, 30)
+            
+            VStack(spacing: 30) {
+                Text("Welcome to WhatsApp")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                
+                Group {
+                    Text("Read our")
+                    + Text(" Privacy Policy")
+                        .foregroundStyle(.blue)
+                    + Text(". Tap Agree and continue to accept the")
+                    + Text(" Terms of Service")
+                        .foregroundStyle(.blue)
+                    + Text(".")
+                }
+                .font(.callout)
+                .foregroundStyle(.secondary)
+                
+                Picker("Select language", selection: $selectedLanguage) {
+                    ForEach(["English", "Spanish"], id: \.self) { language in
+                        Text(language)
+                    }
+                }
+
+                Spacer()
+                
+                Button("Agree and continue") {
+                    
+                }
+            }
+            .padding(.horizontal)
+            
+            Spacer()
+        }
+        .toolbar { toolbar }
+    }
+    
+    @ToolbarContentBuilder
+    private var toolbar: some ToolbarContent {
+        ToolbarItem(placement: .topBarTrailing) {
+            Button("More", systemImage: "ellipsis") {
+                
+            }
+            .tint(.black)
+        }
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
